9cfaf2f6fbc6662834c6425a08ed20612484fbd4
Snehasish Das <dsnehasish73@gmail.com>
2025-07-07 23:09:18 +0530

    .

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = 'user'
-DB_NAME = 'ecommerce'
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "user"
+DB_NAME = "ecommerce"
 SECRET_KEY = '1b83c44289572b7763feea616888ce6cb9e75fb5b79303d118f44abc4c5af7e4'
 
 RAZORPAY_KEY_ID = "rzp_test_TeZssD8bfY472F"
